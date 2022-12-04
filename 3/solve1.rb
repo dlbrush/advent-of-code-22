cc3ba22d57e0b613629a65608f7b9dc17ebc8f01
Devlin Brush <devlinbrush@DevlinB2021-MAC.local>
2022-12-04 12:25:57 -0600

    Combining solutions in 3-1

--- 3/solve1.rb
+++ 3/solve1.rb
@@ -1,18 +1,9 @@
 input = File.open("rucksacks.txt")
 rucksack_lines = input.readlines.map(&:chomp)
 
-# test_data = [
-#   "PdbPPCbhGGpDSDlDDhvDJl", 
-#   "RQNNDpNMSZwvsmqstN", "TTbltCvClzvzCZtwtwLTtQQQgjNgmjgQRRRQSjQLjR", 
-#   "MVcFGTFtqjsJLjjtCd",
-#   "vGWCTDCTQvvtlvQQ",
-#   "FrrbCnqJcbCJcFFbnqJlppCTfQmfmZQTLVswRLZp"
-# ]
-# rucksack_lines = test_data
-
 alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-total_points = 0
+total_points_1 = 0
 
 rucksack_lines.each do |sack|
   half = sack.length / 2
@@ -22,9 +13,26 @@ rucksack_lines.each do |sack|
   # only one item in common expected
   common_item = intersection[0]
   priority_points = alpha.index(common_item) + 1
-  total_points += priority_points
+  total_points_1 += priority_points
   # puts "letter: " + common_item
   # puts "points: " + priority_points.to_s
 end
 
-puts "total: " + total_points.to_s
+puts "total 1: " + total_points_1.to_s
+
+total_points_2 = 0
+group_start = 0
+
+until group_start >= rucksack_lines.length do
+  group = rucksack_lines[group_start, 3]
+  # puts group
+  intersection = group[0].split(//) & group[1].split(//) & group[2].split(//)
+  common_item = intersection[0]
+  priority_points = alpha.index(common_item) + 1
+  total_points_2 += priority_points
+  group_start += 3
+  # puts "letter: " + common_item
+  # puts "points: " + priority_points.to_s
+end
+
+puts "total 2: " + total_points_2.to_s
